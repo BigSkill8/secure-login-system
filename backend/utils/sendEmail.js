9604066c3eb3221f50052f1a8ece63f4966d6e04
Peter Oldpa Kollie Jr <Peterokolliejr@gmail.com>
2026-05-26 22:38:09 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,6 @@
 const emailjs = require("@emailjs/nodejs");
 
-// EMAILJS CREDENTIALS
+// EmailJS credentials
 const SERVICE_ID = "service_etryfes";
 const TEMPLATE_ID = "template_chzqpol";
 const PUBLIC_KEY = "v-2v76AUeztXzwke_";
@@ -9,26 +9,28 @@ const sendEmail = async (to, otp) => {
     try {
         console.log("🔥 EMAILJS OTP FUNCTION CALLED");
 
-        const result = await emailjs.send(
+        const templateParams = {
+            to_email: to,
+            otp: otp
+        };
+
+        const response = await emailjs.send(
             SERVICE_ID,
             TEMPLATE_ID,
-            {
-                to_email: to,
-                otp: otp
-            },
+            templateParams,
             {
                 publicKey: PUBLIC_KEY
             }
         );
 
-        console.log("✅ OTP EMAIL SENT SUCCESSFULLY");
-        console.log("Response:", result.status);
+        console.log("✅ EMAIL SENT SUCCESSFULLY");
+        console.log(response);
 
-        return result;
+        return response;
 
     } catch (error) {
         console.log("❌ EMAILJS ERROR");
-        console.log(error.message);
+        console.log(error?.message || error);
 
         throw new Error("Email sending failed");
     }
