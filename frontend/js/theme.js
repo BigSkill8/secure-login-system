061346b46a6348b1a66668e26cee0824481af01e
Peter Oldpa Kollie Jr <Peterokolliejr@gmail.com>
2026-05-26 14:43:17 +0530

    Update theme.js

--- frontend/js/theme.js
+++ frontend/js/theme.js
@@ -1,1 +1,28 @@
+// APPLY SAVED THEME ON ALL PAGES
+document.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
 
+    if (savedTheme === "light") {
+        document.body.classList.add("light-mode");
+    } else {
+        document.body.classList.add("dark-mode");
+    }
+});
+
+
+// TOGGLE THEME (ALL PAGES)
+const themeBtn = document.getElementById("themeBtn");
+
+if (themeBtn) {
+    themeBtn.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+        document.body.classList.toggle("dark-mode");
+
+        // SAVE STATE
+        if (document.body.classList.contains("light-mode")) {
+            localStorage.setItem("theme", "light");
+        } else {
+            localStorage.setItem("theme", "dark");
+        }
+    });
+}
