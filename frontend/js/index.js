ed844afe9d4ad28a8b12d5e732200ed3392e88fd
Peter Oldpa Kollie Jr <Peterokolliejr@gmail.com>
2026-05-26 15:02:52 +0530

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,67 +1,164 @@
-const loginForm = document.getElementById("loginForm");
-const message = document.getElementById("message");
+const loginForm =
+document.getElementById("loginForm");
+
+const message =
+document.getElementById("message");
+
 
 // LOGIN FORM
-loginForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+loginForm.addEventListener(
+    "submit",
+    async (e) => {
 
-    message.innerHTML = `<div class="loader"></div>`;
+        e.preventDefault();
 
-    try {
-        const response = await fetch(
-            "https://secure-login-system-pp91.onrender.com/api/auth/login",
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
+        const email =
+        document.getElementById("email").value;
+
+        const password =
+        document.getElementById("password").value;
+
+        // SHOW LOADER
+
+        message.innerHTML =
+        `<div class="loader"></div>`;
+
+        try {
+
+            const response = await fetch(
+
+                "http://localhost:5000/api/auth/login",
+
+                {
+
+                    method: "POST",
+
+                    headers: {
+
+                        "Content-Type":
+                        "application/json"
+
+                    },
+
+                    body: JSON.stringify({
+
+                        email,
+
+                        password
+
+                    })
+
+                }
+
+            );
+
+            const data =
+            await response.json();
+
+            // SUCCESS
+
+            if (response.ok) {
+
+                message.innerHTML =
+                `<span class="success">
+                    Login Successful
+                </span>`;
+
+                // SAVE JWT TOKEN
+
+                localStorage.setItem(
+                    "token",
+                    data.token
+                );
+
+                // SAVE USER
+
+                localStorage.setItem(
+                    "username",
+                    data.user.username
+                );
+
+                localStorage.setItem(
+                    "email",
+                    data.user.email
+                );
+
+                // REDIRECT
+
+                setTimeout(() => {
+
+                    window.location.href =
+                    "dashboard.html";
+
+                }, 1500);
+
             }
-        );
 
-        const data = await response.json();
+            // ERROR
 
-        // SUCCESS
-        if (response.ok) {
-            message.innerHTML = `<span class="success">Login Successful</span>`;
+            else {
 
-            // SAVE JWT TOKEN
-            localStorage.setItem("token", data.token);
+                message.innerHTML =
+                `<span class="error">
+                    ${data.message}
+                </span>`;
 
-            // SAVE USER
-            localStorage.setItem("username", data.user.username);
-            localStorage.setItem("email", data.user.email);
+            }
 
-            // REDIRECT
-            setTimeout(() => {
-                window.location.href = "./dashboard.html";
-            }, 1500);
+        } catch (error) {
 
-        } else {
-            message.innerHTML = `<span class="error">${data.message}</span>`;
+            console.log(error);
+
+            message.innerHTML =
+            `<span class="error">
+                Login failed
+            </span>`;
+
         }
 
-    } catch (error) {
-        console.log(error);
-        message.innerHTML = `<span class="error">Login failed</span>`;
     }
-});
+);
 
 
 // SHOW PASSWORD
-const togglePassword = document.getElementById("togglePassword");
-const password = document.getElementById("password");
 
-if (togglePassword && password) {
-    togglePassword.addEventListener("click", () => {
-        password.type = password.type === "password"
-            ? "text"
-            : "password";
-    });
-}
+const togglePassword =
+document.getElementById("togglePassword");
+
+const password =
+document.getElementById("password");
+
+togglePassword.addEventListener(
+    "click",
+    () => {
+
+        if (password.type === "password") {
+
+            password.type = "text";
+
+        } else {
+
+            password.type = "password";
+
+        }
+
+    }
+);
+
+
+// DARK/LIGHT MODE
+
+const themeBtn =
+document.getElementById("themeBtn");
+
+themeBtn.addEventListener(
+    "click",
+    () => {
+
+        document.body.classList.toggle(
+            "light"
+        );
+
+    }
+);
