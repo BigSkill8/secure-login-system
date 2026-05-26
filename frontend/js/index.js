1f4772c0147f2aa7507d570a9379fbd0a1d254c4
Peter Oldpa Kollie Jr <Peterokolliejr@gmail.com>
2026-05-26 13:42:06 +0530

    Fix toggle for dark mode in index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -156,8 +156,7 @@ themeBtn.addEventListener(
     "click",
     () => {
 
-        document.body.classList.toggle(
-            "light"
+       document.body.classList.toggle("dark-mode");
         );
 
     }
